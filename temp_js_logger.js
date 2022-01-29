d7fbe7375696ccc75174456e223b781862530fb9
ogallagher <owengall@icloud.com>
2022-01-29 18:37:47 -0500

    functional console methods patching, conditional suppression by level

--- temp_js_logger.js
+++ temp_js_logger.js
@@ -6,12 +6,43 @@
  */
 
 class TempLogger {
-	constructor({ with_timestamp, caller_name, with_lineno, parse_level_prefix, with_level }) {
+	/**
+	 * Create new TempLogger instance. All args are specified as keys in an options object.
+	 *
+	 * @param {String} level Logging level name.
+	 * @param {boolean} with_timestamp Include timestamp in message.
+	 * @param {String} caller_name Include given caller name in message.
+	 * @param {boolean} with_lineno Include caller line number in message.
+	 * @param {boolean} parse_level_prefix 
+	 * @param {boolean} with_level
+	 */
+	constructor(
+		{
+			level,
+			with_timestamp, 
+			caller_name, 
+			with_lineno, 
+			parse_level_prefix, 
+			with_level 
+		} = {
+			level: TempLogger.STR_DEBUG,
+			with_timestamp: false,
+			caller_name: undefined,
+			with_lineno: true,
+			parse_level_prefix: true,
+			with_level: true
+		}
+	) {
+		this.level = TempLogger.STR_TO_LEVEL[level.toUpperCase()]
+		
 		this.with_timestamp = with_timestamp == true
 		
-		this.caller_name = caller_name
-		if (caller_name == undefined) {
-			this.caller_name = ''
+		this.caller_tag = caller_name
+		if (caller_name == undefined || caller_name == '') {
+			this.caller_tag = ''
+		}
+		else {
+			this.caller_tag += '.'
 		}
 		
 		this.with_lineno = with_lineno == true
@@ -31,30 +62,57 @@ class TempLogger {
 			lineno = TempLogger.get_caller_line()
 		}
 		
-		let level = ''
-		if (this.with_level) {
-			level = TempLogger.LEVEL_DEBUG
-			
-			if (this.parse_level_prefix) {
-				if (typeof data == 'string') {
-					let m = data.match(/^(\w+)\s/)
-					if (m != null) {
-						// m = match str, level str, {index}
-						parsed_level = TempLogger.STR_TO_LEVEL[
-							m[1].toUpperCase()
-						]
-						
-						if (parsed_level != undefined) {
-							level = parsed_level
-						}
+		let level = TempLogger.LEVEL_DEBUG
+		if (this.parse_level_prefix) {
+			// parse level from message prefix
+			if (typeof data == 'string') {
+				let m = data.match(/^(\w+)[\s:]/)
+				
+				if (m != null) {
+					// m = match str, level str, {index}
+					let level_str = m[1].toUpperCase()
+					let parsed_level = TempLogger.STR_TO_LEVEL[level_str]
+					
+					if (parsed_level != undefined) {
+						level = parsed_level
 					}
+					
+					data = data.substring(m.index + m[0].length)
 				}
 			}
 		}
+		else {
+			// convert console method name to level
+			switch (console_method_key) {
+				case 'log':
+					level = TempLogger.LEVEL_DEBUG
+					break
+				
+				case 'info':
+					level = TempLogger.LEVEL_INFO
+					break
+				
+				case 'warn':
+					level = TempLogger.LEVEL_WARNING
+					break
+				
+				case 'error':
+					level = TempLogger.LEVEL_ERROR
+					break
+			}
+		}
 		
-		TempLogger.CONSOLE_METHOD[console_method_key](
-			`${ts} ${this.caller_name}.${level}: ${data}`
-		)
+		// determine whether to suppress or print the message
+		if (level >= this.level) {
+			let level_str = ''
+			if (this.with_level) {
+				level_str = TempLogger.LEVEL_TO_STR[level]
+			}
+		
+			TempLogger.CONSOLE_METHOD[console_method_key](
+				`${ts} ${this.caller_tag}${level_str}: ${data}`
+			)
+		}
 	}
 	
 	// TODO modify for current nesting
@@ -88,10 +146,19 @@ class TempLogger {
 			}
 		}
 	}
+	
+	static config(constructor_opts) {
+		TempLogger.root = new TempLogger(constructor_opts)
+		return TempLogger.root
+	}
 }
 
+// "scoped" class variables
+
 TempLogger.NODE_VERSION = process.version
-TempLogger.NV_MAJOR = parseInt(NODE_VERSION.substr(1, NODE_VERSION.indexOf('.')))
+TempLogger.NV_MAJOR = parseInt(
+	TempLogger.NODE_VERSION.substr(1, TempLogger.NODE_VERSION.indexOf('.'))
+)
 
 TempLogger.CONSOLE_METHOD = {
 	log: console.log,
@@ -111,26 +178,41 @@ TempLogger.STR_INFO = 'INFO'
 TempLogger.STR_WARNING = 'WARNING'
 TempLogger.STR_ERROR = 'ERROR'
 TempLogger.STR_CRITICAL = 'CRITICAL'
+TempLogger.STR_WARN = 'WARN'
+TempLogger.STR_ERR = 'ERR'
 
-TempLogger.STR_TO_LEVEL = {
-	TempLogger.STR_DEBUG: TempLogger.LEVEL_DEBUG,
-	TempLogger.STR_INFO: TempLogger.LEVEL_INFO,
-	TempLogger.STR_WARNING: TempLogger.LEVEL_WARNING,
-	TempLogger.STR_ERROR: TempLogger.LEVEL_ERROR,
-	TempLogger.STR_CRITICAL: TempLogger.LEVEL_CRITICAL
-}
+TempLogger.STR_TO_LEVEL = {}
+TempLogger.STR_TO_LEVEL[TempLogger.STR_DEBUG] = TempLogger.LEVEL_DEBUG
+TempLogger.STR_TO_LEVEL[TempLogger.STR_INFO] = TempLogger.LEVEL_INFO
+TempLogger.STR_TO_LEVEL[TempLogger.STR_WARNING] = TempLogger.LEVEL_WARNING
+TempLogger.STR_TO_LEVEL[TempLogger.STR_WARN] = TempLogger.LEVEL_WARNING
+TempLogger.STR_TO_LEVEL[TempLogger.STR_ERROR] = TempLogger.LEVEL_ERROR
+TempLogger.STR_TO_LEVEL[TempLogger.STR_ERR] = TempLogger.LEVEL_ERROR
+TempLogger.STR_TO_LEVEL[TempLogger.STR_CRITICAL] = TempLogger.LEVEL_CRITICAL
 
-// patch console.log
+TempLogger.LEVEL_TO_STR = {}
+TempLogger.LEVEL_TO_STR[TempLogger.LEVEL_DEBUG] = TempLogger.STR_DEBUG
+TempLogger.LEVEL_TO_STR[TempLogger.LEVEL_INFO] = TempLogger.STR_INFO
+TempLogger.LEVEL_TO_STR[TempLogger.LEVEL_WARNING] = TempLogger.STR_WARNING
+TempLogger.LEVEL_TO_STR[TempLogger.LEVEL_ERROR] = TempLogger.STR_ERROR
+TempLogger.LEVEL_TO_STR[TempLogger.LEVEL_CRITICAL] = TempLogger.STR_CRITICAL
 
 TempLogger.root = new TempLogger()
 
-for (method_key of Object.keys(TempLogger.CONSOLE_METHOD)) {
+// patch console.log
+
+for (let method_key of Object.keys(TempLogger.CONSOLE_METHOD)) {
 	console[method_key] = function(data) {
-		return TempLogger.root(data, method_key)
+		TempLogger.root.log(data, method_key)
 	}
 }
 
 // backend nodejs exports
 if (typeof exports != 'undefined') {
 	exports.TempLogger = TempLogger
+	exports.root = TempLogger.root
+	
+	exports.config = function(opt) {
+		exports.root = TempLogger.config(opt)
+	}
 }
